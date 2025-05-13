ccb780ebe22cf3cc6877d8a7b4970b4537d603a4
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-13 20:52:02 +0700

    chore: minor adjustment to teams atributes

--- entity/team.go
+++ entity/team.go
@@ -5,7 +5,6 @@ import "github.com/google/uuid"
 type Team struct {
 	TeamID       uuid.UUID    `json:"team_id" gorm:"type:varchar(36);primaryKey"`
 	TeamName     string       `json:"team_name" gorm:"type:varchar(50);not null"`
-	Major        string       `json:"major" gorm:"type:varchar(80);not null"`
 	TeamStatus   string       `json:"team_status" gorm:"type:enum('belum terverifikasi', 'terverifikasi');not null"`
 	UserID       uuid.UUID    `json:"user_id"`
 	Registration Registration `json:"registration" gorm:"foreignKey:TeamID"`
