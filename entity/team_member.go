634aff0281784642c134717f60f40f0081e58c48
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-13 22:22:39 +0700

    chore: minor adjustment to team member atributes

--- entity/team_member.go
+++ entity/team_member.go
@@ -3,7 +3,8 @@ package entity
 import "github.com/google/uuid"
 
 type TeamMember struct {
-	TeamMemberID uuid.UUID `json:"team_member_id" gorm:"varchar(36);primaryKey"`
-	MemberName   string    `json:"member_name" gorm:"varchar(70);not null"`
-	TeamID       uuid.UUID `json:"team_id"`
+	TeamMemberID  uuid.UUID `json:"team_member_id" gorm:"varchar(36);primaryKey"`
+	MemberName    string    `json:"member_name" gorm:"varchar(70);not null"`
+	StudentNumber string    `json:"student_number" gorm:"type:varchar(20);"`
+	TeamID        uuid.UUID `json:"team_id"`
 }
