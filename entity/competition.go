8432c2120c9d33265bde3dca3c5e05a333d2c9c4
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-14 23:53:13 +0700

    chore: add new atributes to competition

--- entity/competition.go
+++ entity/competition.go
@@ -1,9 +1,12 @@
 package entity
 
+import "time"
+
 type Competition struct {
-	CompetitionID   int    `json:"competition_id" gorm:"type:int;primaryKey"`
-	CompetitionName string `json:"competition_name" gorm:"type:varchar(70);not null"`
-	Description     string `json:"description" gorm:"type:text;not null"`
+	CompetitionID   int       `json:"competition_id" gorm:"type:int;primaryKey"`
+	CompetitionName string    `json:"competition_name" gorm:"type:varchar(70);not null"`
+	Description     string    `json:"description" gorm:"type:text;not null"`
+	Deadline        time.Time `json:"deadline" gorm:"type:date"`
 
 	Teams          []Team         `gorm:"foreignKey:CompetitionID"`
 	TeamProgresses []TeamProgress `gorm:"foreignKey:CompetitionID"`
