9d944f8f2818c1478ca99eaf5f83a31e03c58bfd
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-18 21:05:57 +0700

    chore: minor adjustment to team progress status

--- entity/team_progress.go
+++ entity/team_progress.go
@@ -9,7 +9,7 @@ import (
 type TeamProgress struct {
 	TeamProgressID int       `json:"team_progress_id" gorm:"int;primaryKey;autoIncrement"`
 	StageID        int       `json:"stage_id"`
-	Status         string    `json:"status" gorm:"type:enum('pending', 'lolos', 'tidak lolos');not null"`
+	Status         string    `json:"status" gorm:"type:enum('diproses', 'lolos', 'tidak lolos');not null"`
 	TeamID         uuid.UUID `json:"team_id"`
 	GdriveLink     string    `json:"gdrive_link" gorm:"varchar(100);not null"`
 	CreatedAt      time.Time `json:"created_at"  gorm:"autoCreateTime"`
