1104ff2acd0f35029298b7525d57ddf4a06c086e
rawhx_x <achmadhasbil24@gmail.com>
2025-06-23 12:04:46 +0700

    feat: update payment status options to include 'ditolak'

--- model/team.go
+++ model/team.go
@@ -53,7 +53,7 @@ type GetTeamMembers struct {
 
 type ReqUpdateStatusTeam struct {
 	TeamID        string `json:"team_id"`
-	PaymentStatus string `json:"payment_status" binding:"oneof='belum terverifikasi' 'terverifikasi'"`
+	PaymentStatus string `json:"payment_status" binding:"oneof='belum terverifikasi' 'terverifikasi' 'ditolak'"`
 }
 
 type TeamInfoResponseAdmin struct {
